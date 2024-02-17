5455e17a4fdd4554194c3933e08e54dbf9ed859e
Hamza Haleem <hamza.haleem@aerq.com>
2024-02-17 11:15:35 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="tweet_data_pipeline",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["dagster", "dagster-cloud"],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
